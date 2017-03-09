04db7ede7fcaabd5f78326062f41229ff37587a0
Francis <1241814583@qq.com>
2017-03-09 21:22:20 +0800

    导包

--- app/src/main/java/com/example/francis/examinationsystem/view/activity/MainActivity.java
+++ app/src/main/java/com/example/francis/examinationsystem/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.francis.examinationsystem;
+package com.example.francis.examinationsystem.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.francis.examinationsystem.R;
 
 public class MainActivity extends AppCompatActivity {
 
